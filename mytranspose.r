36bfbeec11367afe5887b03fbcc146cff5117d7f
YoungjuYoon <78068186+YoungjuYoon@users.noreply.github.com>
2021-04-19 20:51:30 +0900

    Add files via upload

---
+++ mytranspose.r
@@ -0,0 +1,15 @@
+mytranspose <- function(x) {
+  if (is.null(x)== T) {
+    print('null')
+  } else {
+    x <- as.matrix(x)
+    y <- matrix(1, nrow=ncol(x), ncol = nrow(x))
+    for(i in 0:nrow(x)) {
+      for(j in 0:ncol(x)) {
+        y[j,i] <- x[i,j]
+      }
+    }
+    return(y)
+  }
+}
+
